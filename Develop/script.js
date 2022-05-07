44c31414708e3ee65d30a03fedf40d774e7c75b6
Joshua Booth <joshuabenzelli@gmail.com>
2022-05-07 08:03:48 -0600

    Nearly there, needs comments

--- Develop/script.js
+++ Develop/script.js
@@ -3,9 +3,55 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var fullPassword = [];
   var passwordText = document.querySelector("#password");
-
+  var UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  var LowerCase = "abcdefghijklmnopqrstuvwxyz".split('');
+  var Symbols = "!@#$?".split('');
+  var Numbers = "0123456789".split('');
+  var numofchardes = window.prompt("How long would you like the password to be?");
+  var upperyn = window.confirm("Would you like uppercase letters?");
+  var loweryn = window.confirm("Would you like lowercase letters?");
+  var symbolyn = window.confirm("Would you like any symbols?");
+  var numyn = window.confirm("Would you like any numbers?");
+  var numofchar = Number(numofchardes, 10);
+  var password = generatePassword();
+  
+  
+  
+  
+  
+  function generatePassword() {
+    upperyn;
+    loweryn;
+    symbolyn;
+    numyn;
+    while (fullPassword.length <= numofchar) {
+      define();
+    }
+    function define() {
+      
+      if (upperyn === true) {
+        var uind = Math.floor(Math.random() * UpperCase.length);
+        fullPassword.push(UpperCase[uind]);
+      }
+      if (loweryn === true) {
+        var lind = Math.floor(Math.random() * LowerCase.length);
+        fullPassword.push(LowerCase[lind]);
+      }
+      if (symbolyn === true) {
+        var sind = Math.floor(Math.random() * Symbols.length);
+        fullPassword.push(Symbols[sind]);
+      }
+      if (numyn === true) {
+        var nind = Math.floor(Math.random() * Numbers.length);
+        fullPassword.push(Numbers[nind]);
+      }
+    }
+    
+  }
+  passwordIsh = fullPassword.slice(0, numofchar);
+  password = passwordIsh.join("");
   passwordText.value = password;
 
 }
